da98b6e21ed5dfb8c6e7ff7325da52a4ed388386
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2018-01-08 10:43:35 +0100

    response currency when create currency add new error message

--- src/main/kotlin/com/dcs4u/service/CurrencyService.kt
+++ src/main/kotlin/com/dcs4u/service/CurrencyService.kt
@@ -3,7 +3,7 @@ package com.dcs4u.service
 import com.dcs4u.json.request.CurrencyCreationRequest
 import com.dcs4u.model.Currency
 import com.dcs4u.repository.CurrencyRepository
-import com.dcs4u.utils.ErrorMessages.ID_SHOULD_NOT_BE_NULL
+import com.dcs4u.utils.ErrorMessages.CURRENCY_SHOULD_NOT_BE_NULL
 import org.springframework.stereotype.Component
 
 /**
@@ -14,7 +14,7 @@ class CurrencyService(val repository: CurrencyRepository) {
 
     fun get(id: String): Currency? = repository.findById(id).orElse(null)
 
-    fun createCurrency(request: CurrencyCreationRequest): String {
+    fun createCurrency(request: CurrencyCreationRequest): Currency {
         //Destructuring the request
         val (name, symbol, owner) = request
 
@@ -22,6 +22,6 @@ class CurrencyService(val repository: CurrencyRepository) {
         val currency = Currency(name, symbol, owner)
         val savedCurrency = repository.save(currency)
 
-        return savedCurrency.id ?: throw Exception(ID_SHOULD_NOT_BE_NULL)
+        return savedCurrency ?: throw Exception(CURRENCY_SHOULD_NOT_BE_NULL)
     }
 }
